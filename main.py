10aa40ea71d31f34e9d96050b307b778da8b6d71
Elias Pol <eli.pol@mail.toronto.edu>
2020-02-17 00:15:56 -0500

    Add better role detection

--- main.py
+++ main.py
@@ -11,15 +11,23 @@ beano = "<:beano:670475306515169280>"
 
 react_cond = re.compile(".*[b🅱️ ]ean.*")
 bean_role = None
+
+@client.event
+async def on_ready():
+    global bean_role
+    print("Starting up...")
+    for g in client.guilds:
+        for i in g.roles:
+            if i.name == "beaned":
+                bean_role = i
+                print(f"Got beaned role on server {g.name}")
+    print("Done starting!")
+
 @client.event
 async def on_message(mesg):
     global bean_role
     text = mesg.content
     roles = mesg.author.roles
-    if bean_role == None:
-        for i in mesg.guild.roles:
-            if i.name == "beaned":
-                bean_role = i
     if (react_cond.match(text) or (bean_role in roles)):
         await mesg.add_reaction(beano)
 
