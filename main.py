a88a491dc1240428bb0fdb14c9c4768867d9ec46
Elias Pol <eli.pol@mail.toronto.edu>
2020-02-16 23:47:24 -0500

    Add conditon to avoid reaction flooding

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 
 import discord
 
@@ -8,8 +8,13 @@ client = discord.Client()
 
 #to get a specific reaction, simply type \:reaction: in your server
 beano = "<:beano:670475306515169280>"
+
+react_cond = re.compile(".*[b🅱️ ]ean.*")
 @client.event
 async def on_message(mesg):
-    await mesg.add_reaction(beano)
+    text = mesg.content
+    print(text)
+    if (react_cond.match(text)):
+        await mesg.add_reaction(beano)
 
 client.run(token)
