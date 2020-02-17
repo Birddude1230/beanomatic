1d5de14ef0e4669d9ece60cc987d78373cc5e56f
Elias Pol <eli.pol@mail.toronto.edu>
2020-02-16 23:07:36 -0500

    Whoops, wrong beano

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ token = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 #to get a specific reaction, simply type \:reaction: in your server
-beano = "<:beano:678811304034566144>"
+beano = <:beano:670475306515169280>
 @client.event
 async def on_message(mesg):
     await mesg.add_reaction(beano)
