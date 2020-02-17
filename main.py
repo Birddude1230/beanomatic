2b451a30d5ec6f3c9895c0b1e9db949795a0a269
Elias Pol <eli.pol@mail.toronto.edu>
2020-02-17 00:03:55 -0500

    add beaned role

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ client = discord.Client()
 beano = "<:beano:670475306515169280>"
 
 react_cond = re.compile(".*[b🅱️ ]ean.*")
+bean_role = None
 @client.event
 async def on_message(mesg):
+    global bean_role
     text = mesg.content
-    print(text)
-    if (react_cond.match(text)):
+    roles = mesg.author.roles
+    if bean_role == None:
+        for i in mesg.guild.roles:
+            if i.name == "beaned":
+                bean_role = i
+    if (react_cond.match(text) or (bean_role in roles)):
         await mesg.add_reaction(beano)
 
 client.run(token)
