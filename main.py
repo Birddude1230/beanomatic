32d912909e4518ac8bd46c05391ff9ab8350d913
Elias Pol <eli.pol@mail.toronto.edu>
2020-02-16 23:16:29 -0500

    Fix beano formatting

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ token = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 #to get a specific reaction, simply type \:reaction: in your server
-beano = <:beano:670475306515169280>
+beano = "<:beano:670475306515169280>"
 @client.event
 async def on_message(mesg):
     await mesg.add_reaction(beano)
